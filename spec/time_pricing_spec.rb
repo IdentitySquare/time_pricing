386e4ab5753c85c482d7e690bc57100d387a0cfd
Daniel Paul <333233+danielpaul@users.noreply.github.com>
2020-03-31 17:34:40 +0100

    Update and rename spec/time_pricing_spec.rb to test/time_pricing_test.rb

--- spec/time_pricing_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe TimePricing do
-  it "has a version number" do
-    expect(TimePricing::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
